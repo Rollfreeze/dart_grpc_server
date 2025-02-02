31d20d2da7424a5426e35804259d1e756f471435
Egor <71181173+Rollfreeze@users.noreply.github.com>
2025-02-02 22:30:13 +0300

    add address: 'localhost'

--- bin/main.dart
+++ bin/main.dart
@@ -3,6 +3,6 @@ import 'package:dart_grpc_server/greeter_service.dart';
 
 Future<void> main(List<String> arguments) async {
   final server = Server.create(services: [GreeterService()]);
-  await server.serve(port: 50051);
+  await server.serve(address: 'localhost', port: 50051);
   print('Server listening on port ${server.port}...');
 }
