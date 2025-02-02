611b59230229b446755630658b29d72393adeb85
Egor <71181173+Rollfreeze@users.noreply.github.com>
2025-02-02 23:53:47 +0300

    Refactor grpc names

--- lib/greeter_service.dart
+++ lib/greeter_service.dart
@@ -1,9 +1,9 @@
+import 'package:dart_grpc_server/proto/generated/greeter.pbgrpc.dart';
 import 'package:grpc/grpc.dart';
-import 'package:dart_grpc_server/proto/generated/service.pbgrpc.dart';
 
 class GreeterService extends GreeterServiceBase {
   @override
-  Future<HelloReply> sayHello(ServiceCall call, HelloRequest request) async {
-    return HelloReply(message: 'Hello, ${request.name}!');
+  Future<HelloReply> greetSomeone(ServiceCall call, HelloRequest request) async {
+    return HelloReply(message: 'Hello, ${request.name}! My name is Server');
   }
 }
